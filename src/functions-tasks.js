f6c77ed57accef096e5c03e561802214b6475717
nivarnas <rpankratoff@gmail.com>
2024-03-19 18:53:06 +0300

    feat: add solution

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -232,8 +232,12 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let counter = startFrom;
+  return () => {
+    counter += 1;
+    return counter - 1;
+  };
 }
 
 module.exports = {
